3526f02f2ba1875dc05abc82e11cd39296640808
Pummelhead <pummelhead@gmail.com>
2024-03-14 18:05:49 -0600

    fixed primary key and default task when database gets created upon first use

--- db.py
+++ db.py
@@ -9,7 +9,7 @@ cur = conn.cursor()
 
 cur.execute('''CREATE TABLE IF NOT EXISTS all_pending_tasks 
             (
-            task TEXT,
+            task TEXT PRIMARY KEY,
             description TEXT,
             priority INTEGER,
             display_time NULL,
@@ -20,7 +20,7 @@ cur.execute('''CREATE TABLE IF NOT EXISTS all_pending_tasks
             )''')
 cur.execute('''CREATE TABLE IF NOT EXISTS all_completed_tasks 
             (
-            task TEXT,
+            task TEXT PRIMARY KEY,
             description TEXT,
             priority INTEGER,
             display_time NULL,
@@ -31,6 +31,6 @@ cur.execute('''CREATE TABLE IF NOT EXISTS all_completed_tasks
             )''')
 cur.execute('''CREATE TABLE IF NOT EXISTS all_tasks (task TEXT)''')
 cur.execute('''CREATE TABLE IF NOT EXISTS all_created_tables (name TEXT)''')
-cur.execute(f"INSERT INTO all_pending_tasks (task, description, priority) VALUES (?, ?, ?)", ("Task", "description", "priority"))
-cur.execute(f"INSERT INTO all_tasks (task) VALUES ('task')")
+cur.execute(f"INSERT OR IGNORE INTO all_pending_tasks (task, description, priority) VALUES (?, ?, ?)", ("Task", "description", "priority"))
+cur.execute(f"INSERT OR IGNORE INTO all_tasks (task) VALUES ('task')")
 conn.commit()
