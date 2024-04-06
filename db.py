06ed22b806aaa889d122ad17a2436fd99915c2bd
Pummelhead <pummelhead@gmail.com>
2024-04-06 16:09:22 -0600

    updated database table column types

--- db.py
+++ db.py
@@ -12,22 +12,22 @@ cur.execute('''CREATE TABLE IF NOT EXISTS all_pending_tasks
             task TEXT PRIMARY KEY,
             description TEXT,
             priority INTEGER,
-            display_time NULL,
-            due_time NULL,
-            display INTEGER,
-            frequency_step NULL,
-            frequency_step_type NULL
+            display_time TEXT,
+            due_time TEXT,
+            display BOOLEAN,
+            frequency_step INTEGER,
+            frequency_step_type TEXT
             )''')
 cur.execute('''CREATE TABLE IF NOT EXISTS all_completed_tasks 
             (
             task TEXT PRIMARY KEY,
             description TEXT,
             priority INTEGER,
-            display_time NULL,
-            due_time NULL,
-            display INTEGER,
-            frequency_step NULL,
-            frequency_step_type NULL
+            display_time TEXT,
+            due_time TEXT,
+            display BOOLEAN,
+            frequency_step INTEGER,
+            frequency_step_type TEXT
             )''')
 apt_count = cur.execute("SELECT COUNT(*) FROM all_pending_tasks").fetchone()[0]
 cur.execute('''CREATE TABLE IF NOT EXISTS all_created_tables (name TEXT)''')
